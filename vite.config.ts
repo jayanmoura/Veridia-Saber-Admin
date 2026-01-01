75b37bbe28d3f1e97650a4520462208e59c87399
Jayan Moura <jayanmoura@gmail.com>
2025-12-31 22:17:28 -0300

    Forçando porta 3000 no vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,      // Porta para quando você roda 'npm run dev'
+    host: true
+  },
+  preview: {
+    port: 3000,      // <--- IMPORTANTE: Porta para produção (preview)
+    host: true,
+    allowedHosts: true // Libera acesso externo se necessário
+  }
 })
